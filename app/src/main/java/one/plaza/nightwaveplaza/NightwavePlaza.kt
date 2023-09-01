d18ae8052c81451faab896e00c18ed9a8220e798
Alexander Morozov <9192541+potatoru@users.noreply.github.com>
2023-09-01 14:57:55 +1100

    Add ACRA

--- app/src/main/java/one/plaza/nightwaveplaza/NightwavePlaza.kt
+++ app/src/main/java/one/plaza/nightwaveplaza/NightwavePlaza.kt
@@ -3,13 +3,32 @@ package one.plaza.nightwaveplaza
 import android.app.Application
 import com.facebook.drawee.backends.pipeline.Fresco
 import one.plaza.nightwaveplaza.helpers.StorageHelper.initStorage
+import org.acra.config.httpSender
+import org.acra.data.StringFormat
+import org.acra.ktx.initAcra
+import org.acra.sender.HttpSender
 
-class NightwavePlaza: Application() {
+class NightwavePlaza : Application() {
 
     /* Implements onCreate */
     override fun onCreate() {
         super.onCreate()
         Fresco.initialize(this)
         initStorage()
+
+        if (BuildConfig.ACRA_URI.isNotEmpty()) {
+            initAcra {
+                buildConfigClass = BuildConfig::class.java
+                reportFormat = StringFormat.JSON
+
+
+                httpSender {
+                    uri = "https://acra.plaza.one/report"
+                    basicAuthLogin = "cEPWFhhJCQNoRRSu"
+                    basicAuthPassword = "gxezd2UjZ7aNuKj7"
+                    httpMethod = HttpSender.Method.POST
+                }
+            }
+        }
     }
 }
