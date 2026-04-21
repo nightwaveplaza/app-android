e38e97587bc57e74c6463ce6bf537662b4d153f0
Alexander <9192541+potatoru@users.noreply.github.com>
2026-04-21 11:58:47 +1100

    Move to Sentry API

--- app/src/main/java/one/plaza/nightwaveplaza/NightwavePlaza.kt
+++ app/src/main/java/one/plaza/nightwaveplaza/NightwavePlaza.kt
@@ -2,10 +2,6 @@ package one.plaza.nightwaveplaza
 
 import android.app.Application
 import one.plaza.nightwaveplaza.helpers.StorageHelper.initStorage
-import org.acra.config.httpSender
-import org.acra.data.StringFormat
-import org.acra.ktx.initAcra
-import org.acra.sender.HttpSender
 import timber.log.Timber
 
 class NightwavePlaza : Application() {
@@ -17,20 +13,6 @@ class NightwavePlaza : Application() {
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
-        }
-
-        if (BuildConfig.ACRA_URI.isNotEmpty() && !BuildConfig.DEBUG) {
-            initAcra {
-                buildConfigClass = BuildConfig::class.java
-                reportFormat = StringFormat.JSON
-
-                httpSender {
-                    uri = BuildConfig.ACRA_URI
-                    basicAuthLogin = BuildConfig.ACRA_LOGIN
-                    basicAuthPassword = BuildConfig.ACRA_PASS
-                    httpMethod = HttpSender.Method.POST
-                }
-            }
         }
     }
 }
